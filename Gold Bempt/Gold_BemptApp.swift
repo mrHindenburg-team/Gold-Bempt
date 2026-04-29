f77895e23d852e002875fc8261903f6386703398
Tymur Batulin <batulintimur@gmail.com>
2026-04-29 13:54:50 +0200

    Initial Commit

---
+++ Gold Bempt/Gold_BemptApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gold_BemptApp.swift
+//  Gold Bempt
+//
+//  Created by Tymur Batulin on 29.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Gold_BemptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
